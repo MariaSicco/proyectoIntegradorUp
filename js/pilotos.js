97b1b67943197b9ac085816373356f47ba10855e
maria sicco <mjsicco@hotmail.com>
2023-11-24 13:54:48 -0300

    arreglos2

--- js/pilotos.js
+++ js/pilotos.js
@@ -1,21 +1,14 @@
-const slides = document.querySelector('.slides');
-const slideItems = document.querySelectorAll('.slide');
-const slideWidth = slideItems[0].clientWidth;
-let currentPosition = 0;
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
 
-setInterval(() => {
-  currentPosition -= slideWidth;
-  slides.style.transition = 'transform 0.5s ease';
-  slides.style.transform = `translateX(${currentPosition}px)`;
+function showSlide(n) {
+  slides[currentSlide].style.display = 'none';
+  currentSlide = (n + slides.length) % slides.length;
+  slides[currentSlide].style.display = 'block';
+}
 
-  if (currentPosition === -(slideWidth * (slideItems.length - 1))) {
-    setTimeout(() => {
-      slides.style.transition = 'none';
-      currentPosition = 0;
-      slides.style.transform = `translateX(${currentPosition}px)`;
-    }, 500);
-    setTimeout(() => {
-      slides.style.transition = 'transform 0.5s ease';
-    }, 600);
-  }
-}, 3000); // Cambiar cada 3 segundos (ajusta el tiempo según necesites)
+function nextSlide() {
+  showSlide(currentSlide + 1);
+}
+
+setInterval(nextSlide, 3000); // Cambia las imágenes cada 3 segundos
